dea3b6fdb494de2fe51467df6a38fa737f047b18
Shahji umda <shahjiumda747@gmail.com>
2025-03-28 22:16:52 +0530

    Add files via upload

---
+++ rev.js
@@ -0,0 +1,23 @@
+const max = prompt("Enter the max No.");
+
+const random = Math.floor(Math.random() * max) + 1;
+
+let guess = prompt("Guess the number");
+
+while (true) {
+    if (guess == "quit") {
+        console.log("user quit");
+        break;
+    }
+    if (guess == random) {
+        console.log("you are right!!!! congrates!!!! random number was", random);
+        break;
+    }
+    else if (guess <= random) {
+        guess = prompt("your guess was too small !! please try again!! ");
+    }
+    else {
+        guess = prompt("your guess was too large !! please try again!!!");
+    }
+
+}
